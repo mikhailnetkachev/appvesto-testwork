528d82642e2fc6e1a31417cb81441117ccfac95e
Mikhail Netkachev <mikhailnetkachev@MacBook-Pro-Mikhail.local>
2021-03-02 12:14:38 +0200

    Edited configuration: updated dependencies. Edited App component: integrated routing.

--- src/components/templates/App/App.js
+++ src/components/templates/App/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Layout from '../Layout';
+import Home from '../Home';
 
 const App = () => (
-  <Layout>
-    pages is here
-  </Layout>
+  <Router>
+    <Layout>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Layout>
+  </Router>
 );
 
 export default App;
